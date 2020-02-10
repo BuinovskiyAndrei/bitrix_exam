59027a1eee40865491c519ec23e62f284ba31d0a
Andrei Buinovskii <79787468066@yandex.ru>
2020-02-10 10:09:36 +0300

    [ex2-94] Супер инструмент SEO специалиста

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -31,6 +31,20 @@ function functionOnEpilog() {
              "DESCRIPTION" => $APPLICATION->GetCurPage(),
         ));
     }
+    
+    //Супер инструмент СЕО
+    CModule::IncludeModule('iblock');
+    global $APPLICATION;  
+      
+    $arSelect = Array("ID", "NAME", "PROPERTY_TITLE", "PROPERTY_DESCRIPTION");
+    $arFilter = Array("IBLOCK_ID"=>6, "NAME"=>$APPLICATION->GetCurPage(), "ACTIVE"=>"Y");
+    $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+    while($ob = $res->GetNext())
+    {
+
+        $APPLICATION->SetTitle($ob["PROPERTY_TITLE_VALUE"]);
+        $APPLICATION->SetPageProperty("description", $ob["PROPERTY_DESCRIPTION_VALUE"]);
+    }   
 }
 
 AddEventHandler('main', 'OnBeforeEventAdd', Array("MyForm", "my_OnBeforeEventSend"));
